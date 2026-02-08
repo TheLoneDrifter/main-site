74c4c3e1d7b10b043dd7c263eceb1d9cef8a83ba
TheLoneDrifter (GIT) <staydrifting@voltaccept.com>
2026-02-07 19:09:16 -0500

    Fix Error With Font PT2

--- next.config.js
+++ next.config.js
@@ -5,20 +5,7 @@ const nextConfig = {
   },
   images: {
     domains: ['localhost', 'voltaccept.com'],
-  },
-  async headers() {
-    return [
-      {
-        source: '/fonts/(.*)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-    ];
-  },
+  }
 }
 
 module.exports = nextConfig
